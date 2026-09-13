90aa65e81ce9f3b9309eba4667d25c8d88f98478
Alireza Poodineh <itsaeliux@gmail.com>
2026-09-13 12:36:08 +0330

    Add margin to icons in material items

--- app/src/main/java/ir/aeliux/usbtoolkit/widgets/MaterialItem.java
+++ app/src/main/java/ir/aeliux/usbtoolkit/widgets/MaterialItem.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -93,6 +94,12 @@ public class MaterialItem extends ConstraintLayout {
             if (iconRes != 0) {
                 icon.setImageResource(iconRes);
                 icon.setVisibility(View.VISIBLE);
+            } else {
+                ViewGroup.MarginLayoutParams params =
+                        (ViewGroup.MarginLayoutParams) textContainer.getLayoutParams();
+
+                params.setMarginStart(0);
+                textContainer.setLayoutParams(params);
             }
 
             // Configure trailing elements
