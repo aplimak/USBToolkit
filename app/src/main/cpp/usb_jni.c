191b25a80251b4b29223d437ed8fe8ce1a6fed1d
Alireza Poodineh <itsaeliux@gmail.com>
2026-09-24 01:26:03 +0330

    Add more logging to usb jni method

--- app/src/main/cpp/usb_jni.c
+++ app/src/main/cpp/usb_jni.c
@@ -5,6 +5,7 @@
 #include <jni.h>
 #include <usbg/usbg.h>
 #include "dto.h"
+#include "log.h"
 
 JNIEXPORT jint JNICALL
 Java_ir_aeliux_usbtoolkit_Native_usbCreateGadget(JNIEnv *env, jclass cls,
@@ -13,8 +14,10 @@ Java_ir_aeliux_usbtoolkit_Native_usbCreateGadget(JNIEnv *env, jclass cls,
                                                 jobject j_gadgetStrs,
                                                 jobject j_configAttrs,
                                                 jstring j_configStr) {
+    LOGI("Creating Gadget");
     const char *gadget_name = (*env)->GetStringUTFChars(env, j_gadgetName, NULL);
     if (!gadget_name) return -1;
+    LOGV("Gadget name is ok");
 
     int ret;
 
@@ -29,24 +32,31 @@ Java_ir_aeliux_usbtoolkit_Native_usbCreateGadget(JNIEnv *env, jclass cls,
 
     ret = usbtk_usbg_gadget_attrs_from_kotlin(env, j_gadgetAttrs, &g_attrs);
     if (ret != 0) goto cleanup;
+    LOGV("Gadget attrs is ok");
 
     ret = usbtk_usbg_gadget_strs_from_kotlin(env, j_gadgetStrs, &g_strs);
     if (ret != 0) goto cleanup;
+    LOGV("Gadget strs is ok");
 
     ret = usbtk_usbg_config_attrs_from_kotlin(env, j_configAttrs, &c_attrs);
     if (ret != 0) goto cleanup;
+    LOGV("Config attrs is ok");
 
     ret = usbtk_usbg_config_strs_from_jstring(env, j_configStr, &c_strs);
     if (ret != 0) goto cleanup;
+    LOGV("Config strs is ok");
 
     ret = usbg_init("/config", &s);
     if (ret != 0) goto cleanup;
+    LOGD("usbg init done");
 
     ret = usbg_create_gadget(s, gadget_name, &g_attrs, &g_strs, &g);
     if (ret != 0) goto cleanup;
+    LOGD("usbg create_gadget done");
 
-    ret = usbg_create_config(g, 1, "The only one", &c_attrs, &c_strs, &c);
+    ret = usbg_create_config(g, 1, "testconf", &c_attrs, &c_strs, &c);
     if (ret != 0) goto cleanup;
+    LOGD("usbg create_config done");
 
     ret = 0;
 
