7bf14e2d67f622f6ec241863b45986ef9f814874
Alireza Poodineh <itsaeliux@gmail.com>
2026-09-24 09:22:59 +0330

    Add native method java definition

--- app/src/main/java/ir/aeliux/usbtoolkit/Native.java
+++ app/src/main/java/ir/aeliux/usbtoolkit/Native.java
@@ -1,6 +1,9 @@
 package ir.aeliux.usbtoolkit;
 
 import ir.aeliux.usbtoolkit.data.MagicResult;
+import ir.aeliux.usbtoolkit.dto.UsbgConfigAttrs;
+import ir.aeliux.usbtoolkit.dto.UsbgGadgetAttrs;
+import ir.aeliux.usbtoolkit.dto.UsbgGadgetStrs;
 
 public class Native {
     private Native() {}
@@ -12,4 +15,10 @@ public class Native {
     public static native void magicInit(String dbPath);
     public static native MagicResult magicAnalyzeFile(String path);
     public static native void magicRelease();
+
+    private static native int usbCreateGadget(String          gadgetName,
+                                              UsbgGadgetAttrs gadgetAtts,
+                                              UsbgGadgetStrs  gadgetStrs,
+                                              UsbgConfigAttrs configAttrs,
+                                              String          configStrs);
 }
