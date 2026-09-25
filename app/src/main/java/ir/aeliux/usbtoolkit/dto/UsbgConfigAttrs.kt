4414b866623e58a5be24e33ea172ab3ca47d4bb1
Alireza Poodineh <itsaeliux@gmail.com>
2026-09-25 11:54:33 +0330

    Add parcelize

--- app/src/main/java/ir/aeliux/usbtoolkit/dto/UsbgConfigAttrs.kt
+++ app/src/main/java/ir/aeliux/usbtoolkit/dto/UsbgConfigAttrs.kt
@@ -1,6 +1,10 @@
 package ir.aeliux.usbtoolkit.dto
 
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+
+@Parcelize
 data class UsbgConfigAttrs(
     var bmAttributes: Byte,
     var bMaxPower: Byte
-)
+) : Parcelable
